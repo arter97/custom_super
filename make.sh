bf52132c954746def5c3cc9cd378c49181f5ec96
Juhyung Park <qkrwngud825@gmail.com>
2025-01-01 13:37:21 +0900

    3.0-241226
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- make.sh
+++ make.sh
@@ -31,7 +31,7 @@ VENDOR_SIZE=2800
 ACTIVE_SLOT=a
 INACTIVE_SLOT=b
 
-STOCK_FIRMWARE=/home/arter97/android/nothing/3.0/dyn
+STOCK_FIRMWARE=/home/arter97/android/nothing/3.0-241226/dyn
 
 TMP=/tmp/$(uuidgen)
 
