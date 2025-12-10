66fdbf7c065b8d4e3e70afd09345f2689c5ef313
Juhyung Park <qkrwngud825@gmail.com>
2025-12-10 16:05:53 +0900

    S937NKSS4BYKG
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- make.sh
+++ make.sh
@@ -32,7 +32,7 @@ VENDOR_SIZE=5000
 ACTIVE_SLOT=a
 INACTIVE_SLOT=b
 
-STOCK_FIRMWARE=/home/arter97/android/samsung/firmware/s937n/S937NKSS4BYK5/images/dyn
+STOCK_FIRMWARE=/home/arter97/android/samsung/firmware/s937n/S937NKSS4BYKG/images/dyn
 
 TMP=/tmp/$(uuidgen)
 
